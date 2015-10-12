94cae18864335918fab947cff87fba7deab200a2
Matt Parker <mcparker76@gmail.com>
2015-10-11 21:01:31 -0400

    blah

--- NodeLogin/app/src/main/java/com/amal/nodelogin/LoginActivity.java
+++ NodeLogin/app/src/main/java/com/amal/nodelogin/LoginActivity.java
@@ -19,8 +19,6 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-//this is a comment by garcia
-//this is a comment by parker
 public class LoginActivity extends Activity {
     EditText email,password,res_email,code,newpass;
     Button login,cont,cont_code,cancel,cancel1,register,forpass;
