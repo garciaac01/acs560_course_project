805ffb2a0d7364381c0e97418ee6cf4f0431bf40
Matt Parker <mcparker76@gmail.com>
2015-10-28 20:37:17 -0400

    Register adds user to db using httpClient.

--- My EZ Shopper/app/src/main/java/com/amal/nodelogin/RegisterActivity.java
+++ My EZ Shopper/app/src/main/java/com/amal/nodelogin/RegisterActivity.java
@@ -3,20 +3,42 @@ package com.amal.nodelogin;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.AbstractHttpEntity;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class RegisterActivity extends Activity {
@@ -44,39 +66,62 @@ public class RegisterActivity extends Activity {
             }
         });
 
-
         register.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 emailtxt = email.getText().toString();
                 passwordtxt = password.getText().toString();
-                params = new ArrayList<NameValuePair>();
-               params.add(new BasicNameValuePair("email", emailtxt));
-               params.add(new BasicNameValuePair("password", passwordtxt));
-               // params.add(new BasicNameValuePair("name", emailtxt));
-               // params.add(new BasicNameValuePair("description", passwordtxt));
-                ServerRequest sr = new ServerRequest();
-                //JSONObject json = sr.getJSON("http://10.0.2.2:8080/register",params);
-                JSONObject json = sr.getJSON("http://52.91.100.201:8080/register",params);
 
-               // JSONObject json = sr.getJSON("http://52.91.100.201:8080/user", null);
-                //Toast.makeText(getApplication(), json.toString(), Toast.LENGTH_LONG).show();
-                if(json != null){
-                    try{
-                        String jsonstr = json.getString("response");
+                try {
+                    //Added to remove onMainThreadException
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
 
-                        Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+                    HttpClient httpClient = new DefaultHttpClient();
+                    // replace with your url
+                    HttpPost httpPost = new HttpPost("http://52.91.100.201:8080/user");
 
-                        Log.d("Hello", jsonstr);
-                    }catch (JSONException e) {
+
+                    //Post Data
+                    JSONObject obj = new JSONObject();
+                    obj.put("name", emailtxt);
+                    obj.put("password", passwordtxt);
+
+                    //Encoding POST data
+                    try {
+                        AbstractHttpEntity entity = null;
+                        entity = new ByteArrayEntity(obj.toString().getBytes("UTF8"));
+                        entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                        httpPost.setEntity(entity);
+
+                    } catch (UnsupportedEncodingException e) {
+                        // log exception
                         e.printStackTrace();
                     }
-                }
-            }
-        });
-    }
 
+                    //making POST request.
+                    try {
+                        HttpResponse response = httpClient.execute(httpPost);
+                        // write response to log
+                        Log.d("Http Post Response:", response.toString());
+                    } catch (ClientProtocolException e) {
+                        // Log exception
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // Log exception
+                        e.printStackTrace();
+                    }
+
+                } catch (Exception ex){
+
+                }
+
+            }//end onClick
+
+        });//end setOnClickListener
+
+    }//end onCreate
 
 
 
