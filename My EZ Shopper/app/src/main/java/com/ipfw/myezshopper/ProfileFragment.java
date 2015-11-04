88635192c45aed0404f735e229d57d836a7de480
Andy Garcia <garcac01@students.ipfw.edu>
2015-11-03 22:22:48 -0500

    Added fragment for profile, hooked it up to the new deal fragment

---
+++ My EZ Shopper/app/src/main/java/com/ipfw/myezshopper/ProfileFragment.java
@@ -0,0 +1,51 @@
+package com.ipfw.myezshopper;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+
+    private Button goToSearchFragment;
+    private Button goToAddDealFragment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Inflate the layout for this fragment
+
+        goToAddDealFragment = (Button) v.findViewById(R.id.go_to_new_deal_fragment);
+        goToAddDealFragment.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                DealFragment newDeal = new DealFragment();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, newDeal).addToBackStack(null).commit();
+            }
+        });
+        return v;
+    }
+
+
+}
