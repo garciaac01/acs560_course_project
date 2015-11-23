1a2a3044da7613b7be7540772cdee17e508e5492
Matt Parker <mcparker76@gmail.com>
2015-11-22 19:53:57 -0500

    Updated DBHelper

--- My EZ Shopper/app/src/main/java/com/ipfw/myezshopper/DBHelper.java
+++ My EZ Shopper/app/src/main/java/com/ipfw/myezshopper/DBHelper.java
@@ -8,49 +8,49 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "users.db";
-    private static final String TABLE_NAME = "users";
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_PASSWORD = "password";
-    private static final String COLUMN_EMAIL = "email";
+    private static final int USER_DATABASE_VERSION = 1;
+    private static final String USER_DATABASE_NAME = "users.db";
+    private static final String USER_TABLE_NAME = "users";
+    private static final String USER_COLUMN_ID = "id";
+    private static final String USER_COLUMN_NAME = "name";
+    private static final String USER_COLUMN_PASSWORD = "password";
+    private static final String USER_COLUMN_EMAIL = "email";
     SQLiteDatabase db;
+    private String query;
+    private ContentValues values;
+    private Cursor cursor;
 
-    private static final String TABLE_CREATE = "create table " + TABLE_NAME + "(" +
-            COLUMN_ID + " integer primary key not null, " +
-            COLUMN_NAME + " text not null, " +
-            COLUMN_EMAIL + " text not null, " +
-            COLUMN_PASSWORD + " text not null);";
+    private static final String USER_TABLE_CREATE = "create table " + USER_TABLE_NAME + "(" +
+            USER_COLUMN_ID + " integer primary key not null, " +
+            USER_COLUMN_NAME + " text not null, " +
+            USER_COLUMN_EMAIL + " text not null, " +
+            USER_COLUMN_PASSWORD + " text not null);";
 
     public DBHelper(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, USER_DATABASE_NAME, null, USER_DATABASE_VERSION);
     }
 
     public void insertUser(User u){
 
-        db = this.getWritableDatabase();
+        openDatabaseWriteAccess();
 
-        ContentValues values = new ContentValues();
-
-        String query = "SELECT * FROM " + TABLE_NAME;
-        Cursor cursor = db.rawQuery(query,null);
+        query = "SELECT * FROM " + USER_TABLE_NAME;
+        cursor = db.rawQuery(query,null);
 
         int count = cursor.getCount();
-        values.put(COLUMN_ID, count);
-        values.put(COLUMN_NAME, u.getName());
-        values.put(COLUMN_EMAIL, u.getEmail());
-        values.put(COLUMN_PASSWORD, u.getPassword());
-        db.insert(TABLE_NAME, null, values);
+        values.put(USER_COLUMN_ID, count);
+        values.put(USER_COLUMN_NAME, u.getName());
+        values.put(USER_COLUMN_EMAIL, u.getEmail());
+        values.put(USER_COLUMN_PASSWORD, u.getPassword());
+        db.insert(USER_TABLE_NAME, null, values);
         db.close();
     }
 
-
     public String searchPass(String email){
-        db = this.getReadableDatabase();
-        String query = "SELECT " + COLUMN_EMAIL + ", " + COLUMN_PASSWORD + " FROM " + TABLE_NAME;
+        openDatabaseReadAccess();
 
-        Cursor cursor = db.rawQuery(query, null);
+        query = "SELECT " + USER_COLUMN_EMAIL + ", " + USER_COLUMN_PASSWORD + " FROM " + USER_TABLE_NAME;
+        cursor = db.rawQuery(query, null);
 
         String a, b;
         b = "not found";
@@ -63,26 +63,31 @@ public class DBHelper extends SQLiteOpenHelper {
                     break;
                 }
             }while(cursor.moveToNext());
-
         }
 
         return b;
     }
 
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        db.execSQL(TABLE_CREATE);
-
+        db.execSQL(USER_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        String query = "DROP TABLE IF EXISTS " + USER_TABLE_NAME;
         db.execSQL(query);
         onCreate(db);
     }
+
+    private void openDatabaseWriteAccess(){
+        db = this.getWritableDatabase();
+        values = new ContentValues();
+    }
+
+    private void openDatabaseReadAccess(){
+        db = this.getReadableDatabase();
+    }
+
 }
