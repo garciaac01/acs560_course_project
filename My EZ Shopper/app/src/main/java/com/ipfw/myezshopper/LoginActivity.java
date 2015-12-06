ed730eafce5a7cbde9c7e48cfbac22f4451aff19
mcparker76 <mcparker76@gmail.com>
2015-12-06 12:20:53 -0500

    Removed unused imports from LoginActivity.

--- My EZ Shopper/app/src/main/java/com/ipfw/myezshopper/LoginActivity.java
+++ My EZ Shopper/app/src/main/java/com/ipfw/myezshopper/LoginActivity.java
@@ -1,22 +1,14 @@
 package com.ipfw.myezshopper;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,8 +20,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LoginActivity extends Activity {
     PreferencesManager prefManager;
@@ -147,7 +137,6 @@ public class LoginActivity extends Activity {
                     }
 
                     String finalJSON = buffer.toString();
-                    //JSONObject obj = new JSONObject(finalJSON);
                     JSONArray arr = new JSONArray(finalJSON);
                     JSONObject obj = arr.getJSONObject(0);
                     prefManager.setName(obj.getString("name"));
